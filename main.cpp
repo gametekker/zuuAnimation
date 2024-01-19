e29ce71102fb59af65e5933db4e00059ccc98a61
gametekker <104738069+gametekker@users.noreply.github.com>
2024-01-18 20:24:36 -0500

    Update main.cpp
    Remove unnecessary arguments

--- main.cpp
+++ main.cpp
@@ -19,22 +19,7 @@ int main(int argc, char *argv[]){
 
     int resolution = 512; //must be multiple of 32
 
-    // Define a simple colormap
-    std::vector<std::array<double, 3>> colors = {
-        {236.0/255, 244.0/255, 214.0/255},   // Soft Green
-        {154.0/255, 208.0/255, 194.0/255},   // Aquamarine
-        {45.0/255, 149.0/255, 150.0/255},    // Teal
-        {38.0/255, 80.0/255, 115.0/255},     // Deep Sky Blue
-        {34.0/255, 9.0/255, 44.0/255},       // Dark Purple
-        {135.0/255, 35.0/255, 65.0/255},     // Crimson
-        {190.0/255, 49.0/255, 68.0/255},     // Raspberry
-        {240.0/255, 89.0/255, 65.0/255},     // Coral
-        {7.0/255, 102.0/255, 173.0/255},     // Cobalt Blue
-        {41.0/255, 173.0/255, 178.0/255}     // Turquoise
-    };
-    LuminanceColormap cmap (colors, 10);
-
-    render_single_image(y0,yn,x0,xn,resolution,cmap, std::stod(args[0]), std::stod(args[1]), 10);
+    render_frames(y0,yn,x0,xn,resolution, std::stod(args[0]), std::stod(args[1]), 10);
 
 
 
